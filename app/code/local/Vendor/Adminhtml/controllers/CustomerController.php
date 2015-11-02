99fd4f01ac2cb826e6f5b6b55c6e3ab91e7f82ef
Hitesh Yadav <hitmystyle@gmail.com>
2015-11-03 01:24:10 +0530

    initial commit

---
+++ app/code/local/Vendor/Adminhtml/controllers/CustomerController.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Customer admin controller
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+require_once 'Mage/Adminhtml/controllers/CustomerController.php';
+class Vendor_Adminhtml_CustomerController extends Mage_Adminhtml_CustomerController
+{
+
+    protected function _initCustomer($idFieldName = 'id')
+    {
+        $this->_title($this->__('Customers'))->_title($this->__('Manage Customers'));
+
+        $customerId = (int) $this->getRequest()->getParam($idFieldName);
+        $customer = Mage::getModel('customer/customer');
+
+        if ($customerId) {
+            $customer->load($customerId);
+        }
+
+        Mage::register('current_customer', $customer);
+        return $this;
+    }
+
+    /**
+     * Customers list action
+     */
+    public function indexAction()
+    {
+        $this->_title($this->__('Customers'))->_title($this->__('Manage Customers'));
+
+        if ($this->getRequest()->getQuery('ajax')) {
+            $this->_forward('grid');
+            return;
+        }
+        $this->loadLayout();
+
+        /**
+         * Set active menu item
+         */
+        $this->_setActiveMenu('customer/manage');
+
+        /**
+         * Append customers block to content
+         */
+        $this->_addContent(
+            $this->getLayout()->createBlock('adminhtml/customer', 'customer')
+        );
+
+        /**
+         * Add breadcrumb item
+         */
+        $this->_addBreadcrumb(Mage::helper('adminhtml')->__('Customers'), Mage::helper('adminhtml')->__('Customers'));
+        $this->_addBreadcrumb(Mage::helper('adminhtml')->__('Manage Customers'), Mage::helper('adminhtml')->__('Manage Customers'));
+
+        $this->renderLayout();
+    }
+
+    /**
+     * Delete customer action
+     */
+    public function deleteAction()
+    {
+        $this->_initCustomer();
+        $customer = Mage::registry('current_customer');
+        if ($customer->getId()) {
+            try {
+                $customer->load($customer->getId());
+                    $customer->setCustomerStatus('233')
+                    ->save();
+                //$customer->delete();
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('The customer has been disabled.'));
+            }
+            catch (Exception $e){
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+        $this->_redirect('*/customer');
+    }
+
+    public function massDeleteAction()
+    {
+        $customersIds = $this->getRequest()->getParam('customer');
+        if(!is_array($customersIds)) {
+             Mage::getSingleton('adminhtml/session')->addError(Mage::helper('adminhtml')->__('Please select customer(s).'));
+        } else {
+            try {
+                $customer = Mage::getModel('customer/customer');
+                foreach ($customersIds as $customerId) {
+                    $customer->reset()
+                        ->load($customerId)
+                        ->setCustomerStatus('233')
+                        ->save();
+                        //->delete();
+                }
+                Mage::getSingleton('adminhtml/session')->addSuccess(
+                    Mage::helper('adminhtml')->__('Total of %d record(s) were disables.', count($customersIds))
+                );
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+
+        $this->_redirect('*/*/index');
+    }
+
+    public function enableAction(){
+        echo 'enable';exit;
+        $this->_initCustomer();
+        $customer = Mage::registry('current_customer');
+        if ($customer->getId()) {
+            try {
+                $customer->load($customer->getId());
+                    $customer->setCustomerStatus('233')
+                    ->save();
+                //$customer->delete();
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('The customer has been disabled.'));
+            }
+            catch (Exception $e){
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+        $this->_redirect('*/customer');
+
+    }
+}
